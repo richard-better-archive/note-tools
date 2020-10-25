27aa77c32ec903b5af4309ce5c5be829592d73c1
Richard B <git@richardb.me>
2020-10-25 08:07:09 +0100

    prevent playwright errors in detect stage from crashing the export

--- src/roam-export/stage.ts
+++ src/roam-export/stage.ts
@@ -22,45 +22,49 @@ type STAGE =
 Since Roam can be a bit erratic, we always check what stage we're in before doing operation
 */
 export const detectStage = async (page: Page): Promise<STAGE> => {
-  const pageUrl = page.url();
+  try {
+    const pageUrl = page.url();
 
-  const isLoginPage = pageUrl === ROAM_LOGIN_URL;
+    const isLoginPage = pageUrl === ROAM_LOGIN_URL;
 
-  if (isLoginPage) {
-    console.log("stage: LOGGED_OUT");
-    return "LOGGED_OUT";
-  }
+    if (isLoginPage) {
+      console.log("stage: LOGGED_OUT");
+      return "LOGGED_OUT";
+    }
 
-  const isExportDialogVisible = await page.$(SELECTOR_EXPORT_DIALOG);
-  const isExportSpinnerVisible = await page.$(SELECTOR_EXPORT_SPINNER);
+    const isExportDialogVisible = await page.$(SELECTOR_EXPORT_DIALOG);
+    const isExportSpinnerVisible = await page.$(SELECTOR_EXPORT_SPINNER);
 
-  if (isExportDialogVisible && isExportSpinnerVisible) {
-    console.log("stage: EXPORT_IN_PROGRESS");
-    return "EXPORT_IN_PROGRESS";
-  }
+    if (isExportDialogVisible && isExportSpinnerVisible) {
+      console.log("stage: EXPORT_IN_PROGRESS");
+      return "EXPORT_IN_PROGRESS";
+    }
 
-  if (isExportDialogVisible && !isExportSpinnerVisible) {
-    console.log("stage: EXPORT_POPUP");
-    return "EXPORT_POPUP";
-  }
+    if (isExportDialogVisible && !isExportSpinnerVisible) {
+      console.log("stage: EXPORT_POPUP");
+      return "EXPORT_POPUP";
+    }
 
-  const isAppWrapperVisible = await page.$(SELECTOR_ROAM_APP_WRAPPER);
-  const isLoadingIndicatorVisible = await page.$(SELECTOR_LOADING_INDICATOR);
+    const isAppWrapperVisible = await page.$(SELECTOR_ROAM_APP_WRAPPER);
+    const isLoadingIndicatorVisible = await page.$(SELECTOR_LOADING_INDICATOR);
 
-  if (isAppWrapperVisible && !isLoadingIndicatorVisible) {
-    console.log("stage: GRAPH_LOADED");
-    return "GRAPH_LOADED";
-  }
+    if (isAppWrapperVisible && !isLoadingIndicatorVisible) {
+      console.log("stage: GRAPH_LOADED");
+      return "GRAPH_LOADED";
+    }
 
-  if (isLoadingIndicatorVisible && !isAppWrapperVisible) {
-    console.log("stage: LOADING");
-    return "LOADING";
-  }
+    if (isLoadingIndicatorVisible && !isAppWrapperVisible) {
+      console.log("stage: LOADING");
+      return "LOADING";
+    }
 
-  const isGraphSelectionPage = pageUrl === ROAM_GRAPH_SELECTION_URL;
-  if (isGraphSelectionPage) {
-    console.log("stage: GRAPH_SELECT");
-    return "GRAPH_SELECT";
+    const isGraphSelectionPage = pageUrl === ROAM_GRAPH_SELECTION_URL;
+    if (isGraphSelectionPage) {
+      console.log("stage: GRAPH_SELECT");
+      return "GRAPH_SELECT";
+    }
+  } catch {
+    console.log("An error occured during detection");
   }
 
   console.log("stage: UNHANDLED");
