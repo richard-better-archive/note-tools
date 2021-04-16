aea80f420929b3ebacbcc17162d3343bccf20630
Richard B <git@richardb.me>
2021-04-16 20:23:54 +0200

    chore: clean up files

--- src/roam-export/const.ts
+++
@@ -1,7 +0,0 @@
-export const ROAM_LOGIN_URL = "https://roamresearch.com/#/signin";
-export const ROAM_GRAPH_SELECTION_URL = "https://roamresearch.com/#/app";
-
-export const SELECTOR_ROAM_APP_WRAPPER = ".roam-app";
-export const SELECTOR_EXPORT_DIALOG = ".bp3-dialog";
-export const SELECTOR_EXPORT_SPINNER = ".bp3-spinner.bp3-intent-primary";
-export const SELECTOR_LOADING_INDICATOR = ".loading-astrolabe";
